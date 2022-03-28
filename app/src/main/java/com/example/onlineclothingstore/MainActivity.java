e2422f7ff410c85c7ed2e1ddacfaec2c3698fc4a
Jenkin <jenkzyu@gmail.com>
2022-03-28 15:53:23 +0100

    added button to pass intent to login and signup

--- app/src/main/java/com/example/onlineclothingstore/MainActivity.java
+++ app/src/main/java/com/example/onlineclothingstore/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.onlineclothingstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnSignUp,btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignUp = findViewById(R.id.btnSignUp);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(view -> startActivity(
+                new Intent(MainActivity.this, LoginActivity.class)));
+
+        btnSignUp.setOnClickListener(view -> startActivity(
+                new Intent(MainActivity.this, RegisterActivity.class)));
     }
 }
