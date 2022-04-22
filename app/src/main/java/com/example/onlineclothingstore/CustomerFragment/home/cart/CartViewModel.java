76bf33ce40dc3b49ff1a2815aebe426bbfb68457
Jenkin Yu <jenkzyu@gmail.com>
2022-04-22 20:09:49 +0100

    restructured files

--- app/src/main/java/com/example/onlineclothingstore/CustomerFragment/home/cart/CartViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.onlineclothingstore.CustomerFragment.home.cart;
-
-import androidx.lifecycle.ViewModel;
-
-public class CartViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
